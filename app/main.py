5d59d2e4bf7aeb49e41a64139e8dc6d43ece56b6
Анастасия Логвин <anastasialogvin@MacBook-Air-Anastasia-4.local>
2026-03-31 22:01:37 +0300

    added whitespace

--- app/main.py
+++ app/main.py
@@ -32,5 +32,5 @@ class CarWashStation:
 
     def rate_service(self, rate: float) -> None:
         average_rating = (self.average_rating * self.count_of_ratings + rate) / (self.count_of_ratings + 1)
-        self.average_rating = round(average_rating, 1)
+        self.average_rating = round(average_rating, 1 )
         self.count_of_ratings += 1
